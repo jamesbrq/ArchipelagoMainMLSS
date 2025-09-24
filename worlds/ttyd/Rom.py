0eebfb739a0b92fa15da3aa996dd24e54d8c2bba
jamesbrq <jamesbrq@gmail.com>
2025-09-23 22:24:29 -0400

    Update Rom.py

--- worlds/ttyd/Rom.py
+++ worlds/ttyd/Rom.py
@@ -269,11 +269,11 @@ def write_files(world: "TTYDWorld", patch: TTYDProcedurePatch) -> None:
         player_name = world.multiworld.player_name[location.item.player] if location.item is not None else "Unknown Player"
         buffer.write(f"ap_{shop_names[i // 6]}_{i % 6}".encode('utf-8'))
         buffer.write(b'\x00')
-        buffer.write(f"{player_name}'s\n<col {classification_to_color[location.item.classification]}ff>{location.item.name}</col>".encode('utf-8'))
+        buffer.write(f"{player_name}'s\n<col {classification_to_color[get_base_classification(location.item.classification)]}ff>{location.item.name}</col>".encode('utf-8'))
         buffer.write(b'\x00')
     buffer.write(b'\x00')  # null terminator for the end of the table
-
     patch.write_file("desc.txt", buffer.getvalue())
+
     patch.write_file("options.json", json.dumps(options_dict).encode("UTF-8"))
     patch.write_file(f"locations.json", json.dumps(locations_to_dict(world.multiworld.get_locations(world.player))).encode("UTF-8"))
 
@@ -281,3 +281,14 @@ def write_files(world: "TTYDWorld", patch: TTYDProcedurePatch) -> None:
 def locations_to_dict(locations: Iterable[Location]) -> Dict[str, Tuple]:
     return {location.name: (location.item.code, location.item.player) if location.item is not None else (0, 0)
                     for location in locations}
+
+def get_base_classification(classification: ItemClassification = ItemClassification.filler) -> ItemClassification:
+    """Extract the primary classification for color mapping"""
+    if classification & ItemClassification.progression:
+        return ItemClassification.progression
+    elif classification & ItemClassification.trap:
+        return ItemClassification.trap
+    elif classification & ItemClassification.useful:
+        return ItemClassification.useful
+    else:
+        return ItemClassification.filler
