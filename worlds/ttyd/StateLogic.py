59bf1607107577468284af3489d962637ab82a4a
jamesbrq <jamesbrq@gmail.com>
2025-10-11 20:14:51 -0400

    v0.6.1

--- worlds/ttyd/StateLogic.py
+++ worlds/ttyd/StateLogic.py
@@ -1,3 +1,6 @@
+from worlds.ttyd.Data import star_locations
+
+
 def westside(state, player):
     return state.has("Contact Lens", player) or state.has("Bobbery", player) or tube_curse(state, player) or ultra_hammer(state, player)
 
@@ -120,3 +123,6 @@ def sewer_westside_ground(state, player):
 
 def key_any(state, player):
     return state.has("Red Key", player) or state.has("Blue Key", player)
+
+def chapter_completions(state, player, count):
+    return len([location for location in star_locations if state.can_reach(location, "Location", player)]) >= count
