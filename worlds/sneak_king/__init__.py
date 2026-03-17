609cb2ba639d00631528f8eb62afd1151ee15995
jamesbrq <jamesbrq@gmail.com>
2026-03-16 23:35:01 -0400

    Baseline

---
+++ worlds/sneak_king/__init__.py
@@ -0,0 +1,9 @@
+from Options import PerGameCommonOptions, StartInventoryPool, DeathLink
+
+
+class 
+
+
+class SneakKingOptions(PerGameCommonOptions):
+    start_inventory_from_pool = StartInventoryPool
+    death_link: DeathLink
